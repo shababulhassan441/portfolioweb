4e583fcff372ee4a5af5d635d3c6025c7f3aaa92
shabab <shababulhassan441@gmail.com>
2024-11-12 13:28:55 +0500

    remove style atrribute

--- app/data/data.js
+++ app/data/data.js
@@ -709,11 +709,6 @@ const SkeletonThree = () => {
         repeatType: "reverse",
       }}
       className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-gradient-to-r from-[#ee7752] to-[#e73c7e] bg-[length:400%_400%] rounded-lg bg-dot-black/[0.2] flex-col space-y-2"
-      // style={{
-      //   background:
-      //     "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
-      //   backgroundSize: "400% 400%",
-      // }}
     >
       <motion.div className="h-full w-full rounded-lg"></motion.div>
     </motion.div>
